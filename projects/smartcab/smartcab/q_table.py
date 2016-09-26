a3699af73fcf620e5a04702d6fe53c56745995d2
Namory <namorykoulibaly@yahoo.com>
2016-09-25 22:35:20 -0500

    argument conflict

--- projects/smartcab/smartcab/q_table.py
+++ projects/smartcab/smartcab/q_table.py
@@ -16,59 +16,93 @@ class QLTable():
         return self._gamma
         
     def max_q_l(self, light=None, next_waypoint=None, left=None, oncoming=None):
+        
         values =[]
         for action in ['forward', 'left', 'right', 'None']:
             values.append(self.get_value(light=light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=action))
-        return max(values)
+            return max(values)
+    
+    def getQ(self, light=None, next_waypoint=next_waypoint, left=left, oncoming=None, action):
+        return self.q.get((state, action),0.0)
+
+    def learnQ(self, state, action, reward, newv):
+        oldv = self.q.get((state, action), reward)
+        self.q[(state, action)] = (1-self.alpha)*oldv + self.alpha * newv
+
+    def chooseAction(self, state):
+        q = {self.getQ(state, a) for a in ['forward', 'left', 'right', 'None']}
+        maxq = max(q)
+        action = self.actions[maxq]
+        return action
+    def learn(self, state1, action1, reward, state2):
+        maxqnew = max([self.getQ(state2, a) for a in ['forward', 'left', 'right', 'None']])
+        maxqvec=[]        
+        for act in self.valid_actions:
+            maxqvec.append(self.getQ(state2,act))
+            maxqnew=max(maxqvec)
+            self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
+ 
+
 
     def get_value(self, light=None, next_waypoint=None, left=None, oncoming=None, action=None):
+        
         return self._table[self.__state_action(light=light, left=left, oncoming=oncoming, next_waypoint=next_waypoint, action=action)]
 
-    def set_value(self, light=None, next_waypoint=None, left=None, oncoming=None, action=None, next_value=0.0):
-        self._table[self.__state_action(light=light, left=left, oncoming=oncoming, next_waypoint=next_waypoint, action=action)] = next_value
+    def set_value(self, light=None, next_waypoint=None, left=None, oncoming=None, action=None, newv=0.0):
+        
+        self._table[self.__state_action(light=None, next_waypoint=None, left=None, oncoming=None, action=action)] = newv
 
-    def best_action(self, light=None, next_waypoint=None, left=None, oncoming=None):
-        go_to_next_waypoint = self.get_value(light = light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=next_waypoint)
-
-        do_nothing = self.get_value(light=light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=None)
-
-        if go_to_next_waypoint >= do_nothing:
-            return next_waypoint
-        else:
-            return None
 
     def update(self, light=None, next_waypoint=None, left=None, oncoming=None, action=None, reward=0.0):
-        value = self.__value(light=light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=action)
+        
+        oldv = self.__value(light=light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=action)
 
-        next_value = value * (1 - self._alpha) + self._alpha * (reward + self._gamma * value)
+        newv = oldv * (1 - self._alpha) + self._alpha * (reward + self._gamma * oldv)
 
-        self.__set_value(light=light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=action, next_value=next_value)
+        self.__set_value(light=light, next_waypoint=next_waypoint, left=left, oncoming=oncoming, action=action, newv=newv)
+        
+    def argMax(self, light=None, next_waypoint=None, left=None, oncoming=None):
+        # Returns the key with the highest value
+        if len(self.keys()) == 0: return None
+        all = self.items()
+        values = [X[1] for X in all]
+        maxIndex = values.index(max(values))
+        return all[maxIndex][0]
+        
+
 
     def __next_waypoint(self, light, next_waypoint):
         return self._table['light'][light]['next_waypoint'][next_waypoint]
 
-    def __state_action(self, light=None, left=None, oncoming=None, next_waypoint=None, action=None):
+    def __state_action(self, light=None, next_waypoint=None, left=None, oncoming=None, action=None):
         return "{}-{}-{}-{}-{}".format(str(light), str(left), str(oncoming), str(next_waypoint), str(action))
 
     def __initialize_table(self):
         self._table = {}
+        
 
         for light in ['red', 'green']:
             for left in ['forward', 'left', 'right', 'None']:
                 for oncoming in ['forward', 'left', 'right', 'None']:
                     for next_waypoint in ['forward', 'left', 'right', 'None']:
                         for action in ['forward', 'left', 'right', 'None']:
-                            self._table[self.__state_action(light=light, left=left, oncoming=oncoming, next_waypoint=next_waypoint, action=action)] = 0.0
+                            self._table[self.__state_action(light=None, next_waypoint=None, left=None, oncoming=None, action=action)] = 0.0
                                 
 class QLTableUpdater():
-    def __init__(self, table):
+    def __init__(self, table, action_value_table):
         self.table = table
+        self.action_value_table = action_value_table  
         
-    def update(self, light=None, left=None, oncoming=None, next_waypoint=None, action=None, reward=0.0):
+    def update(self, light=None, next_waypoint=None, left=None, oncoming=None, action=None, reward=0.0):
         
-        value = self.table.get_value(light=light, next_waypoint=next_waypoint, oncoming=oncoming, left=left, action=action)
+        light, next_waypoint, left, oncoming = state
         
-        next_value = value*(1 - self.table._alpha) + self.table._alpha * (reward + self.table._gamma*self.table.max_q_l(light=light, oncoming=oncoming, next_waypoint=next_waypoint, left=left))
+        oldv = self.table.get_value(light=light, next_waypoint=next_waypoint, oncoming=oncoming, left=left, action=action)
         
-        self.table.set_value(light=light, left=left, oncoming=oncoming, next_waypoint=next_waypoint, action=action, next_value=next_value)
+        newv = oldv*(1 - self.table._alpha) + self.table._alpha * (reward + self.table._gamma*self.table.max_q_l(light=light, oncoming=oncoming, next_waypoint=next_waypoint, left=left))
+        
+        self.table.set_value(light=light, left=left, oncoming=oncoming, next_waypoint=next_waypoint, action=action, new=newv)
+        
+
+
        
